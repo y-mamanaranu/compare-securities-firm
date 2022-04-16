801764f64d95810beb0c254072ce03d144e8eb14
Yoiduki <90896806+y-muen@users.noreply.github.com>
2022-04-16 14:43:13 +0900

    Add `foegin_etf` to command.

--- compare_securities_firm/cli.py
+++ compare_securities_firm/cli.py
@@ -2,6 +2,9 @@
 
 import click
 import rootpath
+from selenium.webdriver.chrome.options import Options
+
+from .foreign_etf import Foreign_ETF
 
 
 @click.group()
@@ -15,3 +18,13 @@ def root():
     """Return root directory of this package."""
     root = rootpath.detect()
     print(root)
+
+
+@cli.command()
+def foreign_etf():
+    """Update foreign ETF data."""
+    options = Options()
+
+    obj = Foreign_ETF()
+    obj.upate(options=options)
+    obj.update_json()
